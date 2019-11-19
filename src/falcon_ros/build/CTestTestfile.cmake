035462641250418b8b83612859919f49ab613e1e
HCILab <HCILab@wisc.edu>
2019-11-19 13:58:15 -0600

    Remove build folders

--- src/falcon_ros/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hcilab/Documents/PandaFCI/src/falcon_ros
-# Build directory: /home/hcilab/Documents/PandaFCI/src/falcon_ros/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
