dc34b7893dd81674db4034e4904e3357fdc82f14
HCILab <HCILab@wisc.edu>
2019-12-18 15:04:46 -0600

    panda_ros publish robot state

--- src/panda_ros/src/panda_ros.cpp
+++ src/panda_ros/src/panda_ros.cpp
@@ -1,4 +1,8 @@
 #include <ros/ros.h>
+#include <tf2_ros/transform_broadcaster.h>
+#include <tf2/LinearMath/Quaternion.h>
+#include <geometry_msgs/TransformStamped.h>
+#include <sensor_msgs/JointState.h>
 #include <signal.h>
 #include "PandaController.h"
 #include "std_msgs/String.h"
@@ -6,6 +10,8 @@
 #include "geometry_msgs/Twist.h"
 #include "geometry_msgs/Wrench.h"
 #include <relaxed_ik/JointAngles.h>
+#include <eigen3/Eigen/Core>
+#include <eigen3/Eigen/Dense>
 #include <csignal>
 
 using namespace std;
@@ -94,6 +100,70 @@ void callbackCommands(const std_msgs::String& msg){
     
 }
 
+void publishJointState(franka::RobotState robot_state, ros::Publisher jointPub){
+    const vector<string> joint_names{"panda_joint1", "panda_joint2","panda_joint3","panda_joint4","panda_joint5","panda_joint6","panda_joint7"};
+    sensor_msgs::JointState states;
+    states.effort.resize(joint_names.size());
+    states.name.resize(joint_names.size());
+    states.position.resize(joint_names.size());
+    states.velocity.resize(joint_names.size());
+    for (size_t i = 0; i < joint_names.size(); i++) {
+        states.name[i] = joint_names[i];
+    }
+    states.header.stamp = ros::Time::now();
+    for (size_t i = 0; i < joint_names.size(); i++) {
+        states.position[i] = robot_state.q[i];
+        states.velocity[i] = robot_state.dq[i];
+        states.effort[i] = robot_state.tau_J[i];
+    }
+    jointPub.publish(states);
+}
+
+void publishTf(franka::RobotState robot_state){
+    static tf2_ros::TransformBroadcaster br;
+    
+    Eigen::Affine3d transform(Eigen::Matrix4d::Map(robot_state.O_T_EE.data()));
+    Eigen::Vector3d position(transform.translation());
+    Eigen::Quaterniond orientation(transform.linear());
+    geometry_msgs::TransformStamped transformStamped;
+    
+    transformStamped.header.stamp = ros::Time::now();
+    transformStamped.header.frame_id = "base_link";
+    transformStamped.child_frame_id = "end_effector";
+    transformStamped.transform.translation.x = position[0];
+    transformStamped.transform.translation.y = position[1];
+    transformStamped.transform.translation.z = position[2];
+    
+    transformStamped.transform.rotation.x = orientation.coeffs()[0];
+    transformStamped.transform.rotation.y = orientation.coeffs()[1];
+    transformStamped.transform.rotation.z = orientation.coeffs()[2];
+    transformStamped.transform.rotation.w = orientation.coeffs()[3];
+
+    br.sendTransform(transformStamped);
+}
+
+void publishWrench(franka::RobotState robot_state, ros::Publisher wrenchPub){
+    std::array<double, 6> forces;
+    forces = robot_state.O_F_ext_hat_K;
+
+    geometry_msgs::Wrench wrench;
+    wrench.force.x = forces[0];
+    wrench.force.y = forces[1];
+    wrench.force.z = forces[2];
+    wrench.torque.x = forces[3];
+    wrench.torque.y = forces[4];
+    wrench.torque.z = forces[5];
+
+    wrenchPub.publish(wrench);
+}
+
+void publishState(ros::Publisher wrenchPub, ros::Publisher jointPub){
+    franka::RobotState robot_state = PandaController::readRobotState();
+    publishJointState(robot_state, jointPub);
+    publishTf(robot_state);
+    publishWrench(robot_state, wrenchPub);
+}
+
 int main(int argc, char **argv) {
     ros::init(argc, argv, "PandaListener");
     ros::NodeHandle n("~");
@@ -123,6 +193,7 @@ int main(int argc, char **argv) {
     switch(mode){
         case PandaController::ControlMode::CartesianVelocity:
             sub_position = n.subscribe("/panda/cart_vel", 10, updateCallbackCartVel);
+            cout<<"subscribed"<<endl;
             break;
         case PandaController::ControlMode::JointVelocity:
             //TODO: we don't actually have anything that uses this, not set up correctly in PandaController
@@ -137,24 +208,11 @@ int main(int argc, char **argv) {
         case PandaController::ControlMode::None:
             break;     
     }
-
-    franka::RobotState currentState;
-    std::array<double, 6> forces;
     ros::Publisher wrenchPub = n.advertise<geometry_msgs::Wrench>("/panda/wrench", 10);
+    ros::Publisher jointPub = n.advertise<sensor_msgs::JointState>("/panda/joint_states", 1);
     ros::Rate loopRate(1000);
     while (ros::ok() && PandaController::isRunning()) {
-        currentState = PandaController::readRobotState();
-        forces = currentState.O_F_ext_hat_K;
-
-        geometry_msgs::Wrench wrench;
-        wrench.force.x = forces[0];
-        wrench.force.y = forces[1];
-        wrench.force.z = forces[2];
-        wrench.torque.x = forces[3];
-        wrench.torque.y = forces[4];
-        wrench.torque.z = forces[5];
-
-        wrenchPub.publish(wrench);
+        publishState(wrenchPub,jointPub);
         ros::spinOnce();
         loopRate.sleep();
     }
