cf9fa318616eb70894dc06ef15d031ac5f3dc629
Emmanuel Senft <esenft@wisc.edu>
2020-03-02 09:40:04 -0600

    small changes to force dimension for compiling

--- src/PandaController/src/ForceDimension.cpp
+++ src/PandaController/src/ForceDimension.cpp
@@ -25,32 +25,9 @@
 #include <arpa/inet.h> 
 #include <eigen3/Eigen/Geometry>
 
-#define PORT 49152 /* Port the Net F/T always uses */
-#define COMMAND 2 /* Command code 2 starts streaming */
-#define NUM_SAMPLES 1 /* Will send 1 sample before stopping */
-
-/* Typedefs used so integer sizes are more explicit */
-typedef unsigned int uint32;
-typedef int int32;
-typedef unsigned short uint16;
-typedef short int16;
-typedef unsigned char byte;
-typedef struct response_struct {
-	uint32 rdt_sequence;
-	uint32 ft_sequence;
-	uint32 status;
-	int32 FTData[6];
-} RESPONSE;
-// End FT Sensor Socket
 
 using namespace std;
 using namespace std::chrono;
-//using namespace libnifalcon;
-//using namespace StamperKinematicImpl;
-
-//FalconDevice m_falconDevice;
-byte request[8]; /* The request data sent to the Net F/T. */
-int socketHandle;			/* Handle to UDP socket used to communicate with Net F/T. */
 
 array<double,3> workspace_center = {0.42, 0., 0.19};
 array<double, 3> force_dimension = {0.0, 0.0, 0.0};
