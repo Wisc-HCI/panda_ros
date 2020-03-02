f20545c3eb02d9403f28fa915b5469679dc70b58
Emmanuel Senft <esenft@wisc.edu>
2020-03-02 09:40:30 -0600

    dmp deformations start

---
+++ src/dmp_deformations/src/ForceDimension_DMP_ros.cpp
@@ -0,0 +1,386 @@
+#include <cstdlib>
+#include <cstdio>
+#include <iostream>
+#include <unistd.h>
+#include <csignal>
+#include <fstream>
+#include <chrono>
+
+ #include <ros/ros.h>
+ #include "std_msgs/String.h"
+
+// Panda
+#include "PandaController.h"
+#include <franka/robot_state.h>
+
+// Force Dimension
+#include "dhdc.h"
+
+// Socket Libraries for FT sensor readings
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h> 
+#include <eigen3/Eigen/Geometry>
+
+
+using namespace std;
+using namespace std::chrono;
+
+array<double,3> workspace_center = {0.42, 0., 0.19};
+array<double, 3> force_dimension = {0.0, 0.0, 0.0};
+
+std::ofstream outputfile;
+
+void signalHandler(int sig)
+{
+    std::cout << "Interrupt " << sig << " recieved in ForceDimension.cpp\n";
+    //cout << "Closing Force Dimension" << endl;
+    //dhdClose();
+    dhdEnableForce (DHD_OFF);
+    cout << "Closing Panda" << endl;
+    PandaController::stopControl();
+    exit(sig);
+}
+
+bool init_forcedimension() {
+    cout <<"Setting up Force Dimension\n";
+    if (dhdOpen () < 0) {
+        cout << "Unable to open device" << endl;
+        dhdSleep (2.0);
+        return false;
+    }
+    cout << "Force Dimension Setup Complete" << endl;
+
+
+    bool buttonPressed = false;
+
+    cout << endl << endl << "(Press Force Dimension Button to Start Control)" << endl;
+
+    // maybe add timeout?
+    while(!buttonPressed)
+        if(dhdGetButton(0)==1) // button pressed
+        {
+            buttonPressed = true;
+        }
+}
+
+void poll_forcedimension(bool buttonPressed, bool resetCenter, double velcenterx, double velcentery,double velcenterz) {
+
+    // Scaling Values
+    array<double,3> scaling_factors = {-3.0, -3.0, 3.0};
+
+    array<double, 6> panda_pos = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+    
+    double vx=0.0;
+    double vy=0.0;
+    double vz=0.0;
+    double scaling = 0.001;
+
+    if (buttonPressed){
+        array<double, 3> force_dimension_temp = {0.0, 0.0, 0.0};
+        dhdGetPosition (&force_dimension_temp[0], &force_dimension_temp[1], &force_dimension_temp[2]);
+        workspace_center[0]=workspace_center[0]-scaling*(force_dimension_temp[0]-velcenterx);
+        workspace_center[1]=workspace_center[1]-scaling*(force_dimension_temp[1]-velcentery);
+        workspace_center[2]=workspace_center[2]+scaling*(force_dimension_temp[2]-velcenterz);
+
+    }
+
+    else{
+        dhdGetPosition (&force_dimension[0], &force_dimension[1], &force_dimension[2]);
+        if(resetCenter){
+            cout << "Reset Center" << endl;
+            workspace_center[0]=workspace_center[0]-scaling_factors[0]*(force_dimension[0]-velcenterx);
+            workspace_center[1]=workspace_center[1]-scaling_factors[1]*(force_dimension[1]-velcentery);
+            workspace_center[2]=workspace_center[2]-scaling_factors[2]*(force_dimension[2]-velcenterz);
+        }
+    }
+
+    panda_pos[0] = scaling_factors[0] * force_dimension[0] + workspace_center[0];
+    panda_pos[1] = scaling_factors[1] * force_dimension[1] + workspace_center[1];
+    panda_pos[2] = scaling_factors[2] * force_dimension[2] + workspace_center[2];
+
+    //cout << "FD: " << panda_pos[0]  << "," << panda_pos[1] << "," << panda_pos[2] << endl;
+    //cout << "WC: " << workspace_center[0]  << "," << workspace_center[1] << "," << workspace_center[2] << endl;
+    PandaController::writeCommandedPosition(panda_pos);
+
+}
+
+void feedback_forcedimension(double *x, double *y, double *z, double *fx, double *fy, double *fz, bool buttonPressed){
+    franka::RobotState state = PandaController::readRobotState();
+    double scale = -1.0;
+    //cout << "Forces: " << state.O_F_ext_hat_K[0]  << "," << state.O_F_ext_hat_K[1] << "," << state.O_F_ext_hat_K[2] << endl;
+    double vx;
+    double vy;
+    double vz;
+    double b = 30;
+    dhdGetLinearVelocity(&vx,&vy,&vz);
+    if (!buttonPressed){
+        dhdSetForceAndTorque(-state.O_F_ext_hat_K[0] * scale-vx*b,-state.O_F_ext_hat_K[1] * scale-vy*b,state.O_F_ext_hat_K[2] * scale-vz*b,0.0,0.0,0.0);
+    }
+    *x = state.O_T_EE[12];
+    *y = state.O_T_EE[13];
+    *z = state.O_T_EE[14];
+    *fx = -state.O_F_ext_hat_K[0];
+    *fy = -state.O_F_ext_hat_K[1];
+    *fz = -state.O_F_ext_hat_K[2];
+}
+
+void log_demonstration(double x, double y, double z, double fx, double fy, double fz){
+    outputfile << x << "," << y << "," << z << "," << fx << "," << fy << "," << fz << "\n";
+}
+
+void feedback_ft_forcedimension(bool buttonPressed, double velcenterx, double velcentery, double velcenterz,
+double *x, double *y, double *z, double *fx, double *fy, double *fz){
+
+    double vx;
+    double vy;
+    double vz;
+    double b = 50; // injected damping
+    double scale = -0.4; //force scaling
+    double vel_mode_stiffness = 5000;
+
+    double fd_x;
+    double fd_y;
+    double fd_z;
+
+    dhdGetLinearVelocity(&vx,&vy,&vz);
+    dhdGetPosition (&fd_x, &fd_y, &fd_z);
+    std::array<double, 6> FTData = PandaController::readFTForces();
+    
+    Eigen::VectorXd v = Eigen::Map<Eigen::VectorXd>(FTData.data(),3);
+
+    Eigen::Matrix3d m;
+    m = Eigen::AngleAxisd(-M_PI/6, Eigen::Vector3d::UnitZ());
+    v = m * v;
+
+    FTData[0] = v[0];
+    FTData[1] = v[1];
+    FTData[2] = v[2];
+
+
+    if(!buttonPressed){ 
+        dhdSetForceAndTorque(FTData[0] * scale -vx*b,-FTData[1]* scale -vy*b,FTData[2]* scale -vz*b,0.0,0.0,0.0);
+    }
+
+    else{
+        dhdSetForceAndTorque(-vel_mode_stiffness*(fd_x-velcenterx)*abs(fd_x-velcenterx),-vel_mode_stiffness*(fd_y-velcentery)*abs(fd_y-velcentery),
+        -vel_mode_stiffness*(fd_z-velcenterz)*abs(fd_z-velcenterz),0.0,0.0,0.0);
+    }
+    
+    franka::RobotState state = PandaController::readRobotState();
+    *x = state.O_T_EE[12];
+    *y = state.O_T_EE[13];
+    *z = state.O_T_EE[14];
+    *fx = -state.O_F_ext_hat_K[0];
+    *fy = -state.O_F_ext_hat_K[1];
+    *fz = -state.O_F_ext_hat_K[2];
+
+    // TODO: Fix this to be the forces from the actual force torque sensor
+
+    //cout << "FT: " << -FTData[0] * scale -vx*b  << "," << -FTData[1]* scale << "," << FTData[2]* scale -vz*b << endl;
+
+}
+
+int main(int argc, char **argv) {
+    //Setup the signal handler for exiting
+    signal(SIGINT, signalHandler);
+
+    ros::init(argc, argv, "ForceDimensionDMP");
+    ros::NodeHandle n("~");  
+    ros::Publisher file_pub = 
+        n.advertise<std_msgs::String>("/dmp/filepub", 5);
+    ros::Publisher reset_pub = 
+        n.advertise<std_msgs::String>("/dmp/reset", 5);
+
+    //setup_ft();
+
+    if (!init_forcedimension()) {
+        cout << endl << "Failed to init force dimension" << endl;
+        return -1;
+    }
+
+
+    // Turn on gravity compensation
+    dhdEnableForce (DHD_ON);
+
+    // Second button press to start control & forces
+    bool buttonReleased=false;
+    while(!buttonReleased)
+        if(dhdGetButton(0)==0) // button released
+        {
+            buttonReleased = true;
+        }
+
+    cout << "Press Button again to enable Forces and Panda Control" << endl;
+
+    bool buttonPressed=false;
+    while(!buttonPressed)
+        if(dhdGetButton(0)==1) // button pressed
+        {
+            buttonPressed = true;
+        }
+    
+    buttonReleased=false;
+    while(!buttonReleased)
+        if(dhdGetButton(0)==0) // button released
+        {
+            buttonReleased = true;
+        }
+
+
+    // Start Panda controller and poll force dimension to make sure it has reasonable starting values
+    pid_t pid = PandaController::initPandaController(PandaController::ControlMode::CartesianPosition);
+    if (pid < 0) {
+       cout << "Failed to start panda process" << endl;
+    }
+    poll_forcedimension(false,false,0.0,0.0,0.0);
+
+    // State flow based on button presses
+    bool exit=false;
+    bool start_recording = false;
+
+    // Initialize data for storage
+    double x = 0;
+    double y = 0;
+    double z = 0;
+    double fx = 0;
+    double fy = 0;
+    double fz = 0;
+
+    double velcenterx = 0;
+    double velcentery = 0;
+    double velcenterz = 0;
+    
+    buttonPressed = false;
+    bool velocity_mode = false;
+    auto start = high_resolution_clock::now(); 
+    bool gripping = false;
+    bool recording = false;
+    bool resetCenter = false;
+
+    int file_iter = 0;
+
+    while (PandaController::isRunning() && ros::ok()) {
+        poll_forcedimension(velocity_mode,resetCenter, velcenterx,velcentery,velcenterz);
+
+        if (resetCenter){ //only allow one correction
+            resetCenter=false;
+        }
+        //feedback_forcedimension(&x,&y,&z,&fx,&fy,&fz,buttonPressed);
+        feedback_ft_forcedimension(velocity_mode, velcenterx, velcentery, velcenterz,&x,&y,&z,&fx,&fy,&fz);
+
+
+        // If button pressed and released in less than 0.3 seconds,
+        // it is a gripping action
+
+        // If held greater than 0.3 seconds, it is a velocity control action
+
+        if(!buttonPressed && dhdGetButton(0)==1) // button initially pressed
+        {
+            buttonPressed = true;
+            start = high_resolution_clock::now(); 
+        }
+
+        
+        if(buttonPressed) // button held
+        {
+            auto end = high_resolution_clock::now(); 
+            auto duration = duration_cast<microseconds>(end - start); 
+
+            if(duration.count()>=300000)
+            {
+                if (velocity_mode==false)
+                {
+                    velocity_mode=true;
+                    // When starting a potential velocity mode action
+                    // Need to see where the current center value is
+                    dhdGetPosition (&velcenterx, &velcentery, &velcenterz);
+                    cout << "Velocity Mode" << endl;
+                }
+               
+            }
+            
+        }
+
+        if(buttonPressed && dhdGetButton(0)==0) // button released
+        {
+            auto end = high_resolution_clock::now(); 
+            auto duration = duration_cast<microseconds>(end - start); 
+
+            if(duration.count()<300000)
+            {
+                cout << "GRIPPER ACTION" << endl;
+                if(!gripping){
+                    PandaController:: graspObject();
+                    gripping=true;
+                }
+                else{
+                    PandaController::releaseObject();
+                    gripping=false;
+                }
+            }
+
+            cout << "Button Unpressed" << endl;
+            buttonPressed=false;
+        
+            if (velocity_mode==true){
+                // Deal with discontinuity
+                double tempx;
+                double tempy;
+                double tempz;
+                resetCenter=true;
+
+                velocity_mode=false;
+            }
+        }
+
+        if (dhdKbHit()) {
+            
+            char keypress = dhdKbGet();
+            if (keypress == 'r'){
+                if(recording==false){
+                    string filename = {"panda_demo_"+to_string(file_iter)+".csv"};
+                    remove( filename.c_str() );
+                    outputfile.open (filename.c_str());
+                    cout << "Starting Recording: " << filename.c_str() <<  endl;
+                    file_iter++;
+                    recording=true;
+                }
+                else{
+                    outputfile.close();
+                    cout << "Ending Recording" << endl;
+                    recording=false;
+                    // Let ROS know a file has been published to update the DMP
+                    file_pub.publish("panda_demo_"+to_string(file_iter-1)+".csv");
+                }
+            
+            }
+
+            if (keypress == 'x'){
+                reset_pub.publish('Reset');
+                cout << "RESET DMP" << endl;
+                } 
+        }
+
+        if (recording){
+            log_demonstration(x,y,z,fx,fy,fz);
+        }
+        
+    ros::spinOnce();
+
+      
+    }
+
+    dhdEnableForce (DHD_OFF);
+    cout << "Closing Panda" << endl;
+    PandaController::stopControl();
+    dhdClose();
+    
+    return 0;
+}
---
+++ src/dmp_deformations/src/panda_ros.cpp
@@ -0,0 +1,222 @@
+#include <ros/ros.h>
+#include <tf2_ros/transform_broadcaster.h>
+#include <tf2/LinearMath/Quaternion.h>
+#include <geometry_msgs/TransformStamped.h>
+#include <sensor_msgs/JointState.h>
+#include <signal.h>
+#include "PandaController.h"
+#include "std_msgs/String.h"
+#include "geometry_msgs/Pose.h"
+#include "geometry_msgs/Twist.h"
+#include "geometry_msgs/Wrench.h"
+#include <relaxed_ik/JointAngles.h>
+#include <eigen3/Eigen/Core>
+#include <eigen3/Eigen/Dense>
+#include <csignal>
+
+using namespace std;
+
+void signalHandler(int sig)
+{
+    std::cout << "Interrupt " << sig << " recieved in panda_ros.cpp\n";
+    PandaController::stopControl();
+    ros::NodeHandle n("~");
+    ros::Publisher wrenchPub = n.advertise<geometry_msgs::Wrench>("/panda/wrench", 10);
+    geometry_msgs::Wrench wrench;
+    wrench.force.x = 0;
+    wrench.force.y = 0;
+    wrench.force.z = 0;
+    wrench.torque.x = 0;
+    wrench.torque.y = 0;
+    wrench.torque.z = 0;
+
+    wrenchPub.publish(wrench);   
+    ros::shutdown();
+
+    std::array<double, 6> data = {0.0,0.0,0.0,0.0,0.0,0.0};
+    PandaController::writeCommandedVelocity(data);
+    exit(sig);
+}
+
+void updateCallbackCartPos(const geometry_msgs::Pose::ConstPtr& msg){
+    if (PandaController::isRunning()){
+        std::array<double, 6> position;
+        position[0] = msg->position.x;
+        position[1] = msg->position.y;
+        position[2] = msg->position.z;
+        //TODO update with quat
+        position[3] = 0;
+        position[4] = 0;
+        position[5] = 0;
+        
+        PandaController::writeCommandedPosition(position);
+    }
+}
+
+void updateCallbackJointPos(const relaxed_ik::JointAngles::ConstPtr& msg){
+    if (PandaController::isRunning()){
+        std::array<double, 7> position;
+        position[0] = msg->angles.data[0];
+        position[1] = msg->angles.data[1];
+        position[2] = msg->angles.data[2];
+        position[3] = msg->angles.data[3];
+        position[4] = msg->angles.data[4];
+        position[5] = msg->angles.data[5];
+        position[6] = msg->angles.data[6];
+        PandaController::writeJointAngles(position);
+    }
+}
+
+void updateCallbackJointVel(const relaxed_ik::JointAngles::ConstPtr& msg){
+    //TODO
+    return;
+}
+
+void updateCallbackCartVel(const geometry_msgs::Twist::ConstPtr& msg){
+    if (PandaController::isRunning()){
+        std::array<double, 6> velocity;
+        velocity[0] = msg->linear.x;
+        velocity[1] = msg->linear.y;
+        velocity[2] = msg->linear.z;
+        velocity[3] = msg->angular.x;
+        velocity[4] = msg->angular.y;
+        velocity[5] = msg->angular.z;
+        PandaController::writeCommandedVelocity(velocity);
+    }
+}
+
+void callbackCommands(const std_msgs::String& msg){
+
+    if(msg.data == "grasp"){
+        cout<<"Grasping"<<endl;
+        PandaController::graspObject();
+    }
+    if(msg.data == "release"){
+        PandaController::releaseObject();
+    }
+    if(msg.data == "toggleGrip") {
+        PandaController::toggleGrip();
+    }
+    
+}
+
+void publishJointState(franka::RobotState robot_state, ros::Publisher jointPub){
+    const vector<string> joint_names{"panda_joint1", "panda_joint2","panda_joint3","panda_joint4","panda_joint5","panda_joint6","panda_joint7"};
+    sensor_msgs::JointState states;
+    states.effort.resize(joint_names.size());
+    states.name.resize(joint_names.size());
+    states.position.resize(joint_names.size());
+    states.velocity.resize(joint_names.size());
+    for (size_t i = 0; i < joint_names.size(); i++) {
+        states.name[i] = joint_names[i];
+    }
+    states.header.stamp = ros::Time::now();
+    for (size_t i = 0; i < joint_names.size(); i++) {
+        states.position[i] = robot_state.q[i];
+        states.velocity[i] = robot_state.dq[i];
+        states.effort[i] = robot_state.tau_J[i];
+    }
+    jointPub.publish(states);
+}
+
+void publishTf(franka::RobotState robot_state){
+    static tf2_ros::TransformBroadcaster br;
+    
+    Eigen::Affine3d transform(Eigen::Matrix4d::Map(robot_state.O_T_EE.data()));
+    Eigen::Vector3d position(transform.translation());
+    Eigen::Quaterniond orientation(transform.linear());
+    geometry_msgs::TransformStamped transformStamped;
+    
+    transformStamped.header.stamp = ros::Time::now();
+    transformStamped.header.frame_id = "panda_link0";
+    transformStamped.child_frame_id = "end_effector";
+    transformStamped.transform.translation.x = position[0];
+    transformStamped.transform.translation.y = position[1];
+    transformStamped.transform.translation.z = position[2];
+    
+    transformStamped.transform.rotation.x = orientation.coeffs()[0];
+    transformStamped.transform.rotation.y = orientation.coeffs()[1];
+    transformStamped.transform.rotation.z = orientation.coeffs()[2];
+    transformStamped.transform.rotation.w = orientation.coeffs()[3];
+
+    br.sendTransform(transformStamped);
+}
+
+void publishWrench(franka::RobotState robot_state, ros::Publisher wrenchPub){
+    std::array<double, 6> forces;
+    //forces = robot_state.O_F_ext_hat_K;
+    forces = PandaController::readFTForces();
+
+    geometry_msgs::Wrench wrench;
+    wrench.force.x = forces[0];
+    wrench.force.y = forces[1];
+    wrench.force.z = forces[2];
+    wrench.torque.x = forces[3];
+    wrench.torque.y = forces[4];
+    wrench.torque.z = forces[5];
+
+    wrenchPub.publish(wrench);
+}
+
+void publishState(ros::Publisher wrenchPub, ros::Publisher jointPub){
+    franka::RobotState robot_state = PandaController::readRobotState();
+    publishJointState(robot_state, jointPub);
+    publishTf(robot_state);
+    publishWrench(robot_state, wrenchPub);
+}
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "PandaListener");
+    ros::NodeHandle n("~");
+    std::string mode_str;
+    //Always specify parameter, it uses cached parameter instead of default value.
+    n.param<std::string>("control_mode", mode_str,"none");
+    PandaController::ControlMode mode;
+
+    if(mode_str == "cartesian_velocity")
+        mode = PandaController::ControlMode::CartesianVelocity;
+    if(mode_str == "joint_velocity")
+        mode = PandaController::ControlMode::JointVelocity;
+    if(mode_str == "cartesian_position")
+        mode = PandaController::ControlMode::CartesianPosition;
+    if(mode_str == "joint_position")
+        mode = PandaController::ControlMode::JointPosition;
+    if(mode_str == "none")
+        mode = PandaController::ControlMode::None;
+        
+    //Setup the signal handler for exiting, must be called after ros is intialized
+    signal(SIGINT, signalHandler); 
+
+    PandaController::initPandaController(mode);
+    
+    ros::Subscriber sub_commands = n.subscribe("/panda/commands", 10, callbackCommands);
+    ros::Subscriber sub_position;
+    switch(mode){
+        case PandaController::ControlMode::CartesianVelocity:
+            sub_position = n.subscribe("/panda/cart_vel", 10, updateCallbackCartVel);
+            cout<<"subscribed"<<endl;
+            break;
+        case PandaController::ControlMode::JointVelocity:
+            //TODO: we don't actually have anything that uses this, not set up correctly in PandaController
+            sub_position = n.subscribe("/relaxed_ik/joint_angle_solutions", 10, updateCallbackJointVel);
+            break;
+        case PandaController::ControlMode::CartesianPosition:
+            sub_position = n.subscribe("/panda/cart_pose", 10, updateCallbackCartPos);
+            break;
+        case PandaController::ControlMode::JointPosition:
+            sub_position = n.subscribe("/relaxed_ik/joint_angle_solutions", 10, updateCallbackJointPos);
+            break;
+        case PandaController::ControlMode::None:
+            break;     
+    }
+    ros::Publisher wrenchPub = n.advertise<geometry_msgs::Wrench>("/panda/wrench", 10);
+    ros::Publisher jointPub = n.advertise<sensor_msgs::JointState>("/panda/joint_states", 1);
+    ros::Rate loopRate(1000);
+    while (ros::ok() && PandaController::isRunning()) {
+        publishState(wrenchPub,jointPub);
+        ros::spinOnce();
+        loopRate.sleep();
+    }
+
+    return 0;
+}
