5496eb69f0aed0a436d0c93f66020de201924ab5
Mike <mhagenow@wisc.edu>
2020-07-16 13:57:07 -0500

    more updates to bspline fitting code

--- src/dmp_deformations/nodes/solidworks_to_bsplinesurface.py
+++ src/dmp_deformations/nodes/solidworks_to_bsplinesurface.py
@@ -34,25 +34,33 @@ def obj_plane_dist(x, pts):
     # print x[0], x[1], x[2], sum
     return sum
 
+
 def findPlane(pts):
-    wx = 0.0
-    wy = 0.0
-    d =  0.0
+    best_opt = np.inf
+    for ii in range(0,10):
+        wx = np.random.rand()*np.pi/2
+        wy = np.random.rand()*np.pi/2
+        d = 1.0*(np.random.rand()-0.5)
 
-    # TODO: add random restarts for local minima...
+        x0 = np.array([wx, wy, d])
+        bounds = Bounds([0.0, 0.0, -np.inf], [np.pi, np.pi, np.inf])
+        res = minimize(obj_plane_dist, x0, method='Nelder-Mead',
+                       options={'disp': False}, bounds=None, args=(pts))
 
-    x0 = np.array([wx, wy, d])
-    bounds = Bounds([0.0, 0.0, -np.inf], [np.pi, np.pi, np.inf])
-    res = minimize(obj_plane_dist, x0, method='Nelder-Mead',
-                   options={'disp': False}, bounds=None, args=(pts))
-    return res.x[0], res.x[1], res.x[2]
+        if res.fun < best_opt:
+            best_opt=res.fun
+            wx_best = res.x[0]
+            wy_best = res.x[1]
+            d_best = res.x[2]
+
+    return wx_best, wy_best, d_best
 
 def main():
     stl_file = '/home/mike/Desktop/NASAULIlayupFLANGEsurface.STL'
     mesh = trimesh.load(stl_file)
 
     # Turn STL into a point cloud of the desired surface using even sampling
-    points, something = sample.sample_surface_even(mesh,400)
+    points, something = sample.sample_surface_even(mesh,200)
 
     # convert properly for mm to m issue
     points = points/1000
@@ -84,23 +92,84 @@ def main():
         projected_pts.append([u_temp, v_temp])
     projected_pts = np.array(projected_pts)
 
+
     #######################################################
     # Fit the best rectangle around the points            #
     #######################################################
     # use something like this... https://gis.stackexchange.com/questions/22895/finding-minimum-area-rectangle-for-given-points
+    # key idea: orientation of minimized rectangle is the same as one of the edges of the point cloud convex hull
+    # get the direction and the normal direction
+    # using all points, find the min and max of these
     fig = plt.figure()
     plt.scatter(projected_pts[:, 0], projected_pts[:, 1])
     hull = ConvexHull(projected_pts)
-    for simplex in hull.simplices:
-        plt.plot(projected_pts[simplex, 0], projected_pts[simplex, 1], 'k-', color='red')
+    # for simplex in hull.simplices:
+    #     print "simplex:",simplex
+    #     plt.plot(projected_pts[simplex, 0], projected_pts[simplex, 1], 'k-', color='red')
 
-    # key idea: orientation of minimized rectangle is the same as one of the edges of the point cloud convex hull
+    min_area_temp = np.inf
 
 
-    # create evenly spaced candidates
+    # each simplex is a set of indices representing an edge in the convex hull
+    for simplex_id in hull.simplices:
+        plt.plot([projected_pts[simplex_id[0]][0],projected_pts[simplex_id[1]][0]], [projected_pts[simplex_id[0]][1],projected_pts[simplex_id[1]][1]], 'k-', color='red')
 
-    # conform back to the surface as control points (Do I need a spring)
+        min_edge = np.inf
+        max_edge = -np.inf
+        min_orth_edge = np.inf
+        max_orth_edge = -np.inf
 
+        # projection stuff for the rectangle
+        # get vector from point 1 to point 2
+        edge_vec = projected_pts[simplex_id[1]]-projected_pts[simplex_id[0]]
+        edge_vec = edge_vec/np.linalg.norm(edge_vec)
+
+        # 90 degree rotation CW
+        orth_edge_vec = np.array([edge_vec[1], -edge_vec[0]])
+
+        for vertex in hull.vertices:
+            # get vector from point 1 to point n
+            vec_to_n = projected_pts[vertex]-projected_pts[simplex_id[0]]
+
+            # along axis_proj
+            dist_temp_axis = np.dot(vec_to_n,edge_vec)
+            if dist_temp_axis<min_edge:
+                min_edge = dist_temp_axis
+            if dist_temp_axis>max_edge:
+                max_edge = dist_temp_axis
+
+            # orthogonal axis projection
+            dist_temp_orth = np.dot(vec_to_n, orth_edge_vec)
+            if dist_temp_orth < min_orth_edge:
+                min_orth_edge = dist_temp_orth
+            if dist_temp_orth > max_orth_edge:
+                max_orth_edge = dist_temp_orth
+
+
+
+        # Calculate area
+        area_temp = (max_edge-min_edge)*(max_orth_edge-min_orth_edge)
+
+        if area_temp<min_area_temp:
+            min_area_temp=area_temp
+            pt_1 = projected_pts[simplex_id[0]]+min_edge*edge_vec+min_orth_edge*orth_edge_vec
+            pt_2 = projected_pts[simplex_id[0]]+max_edge*edge_vec+min_orth_edge*orth_edge_vec
+            pt_3 = projected_pts[simplex_id[0]] + max_edge * edge_vec + max_orth_edge * orth_edge_vec
+            pt_4 = projected_pts[simplex_id[0]] + min_edge * edge_vec + max_orth_edge * orth_edge_vec
+
+
+
+    # PLOT THE RECTANGLE!!!!
+    plt.plot([pt_1[0], pt_2[0]],[pt_1[1], pt_2[1]], color='green')
+    plt.plot([pt_2[0], pt_3[0]], [pt_2[1], pt_3[1]], color='green')
+    plt.plot([pt_3[0], pt_4[0]], [pt_3[1], pt_4[1]], color='green')
+    plt.plot([pt_4[0], pt_1[0]], [pt_4[1], pt_1[1]], color='green')
+
+
+
+    # create evenly spaced candidates along this new grid
+
+    # conform back to the surface as control points (Do I need a spring - start wo it!)
 
     # print points
     # print(np.shape(points))
@@ -109,7 +178,7 @@ def main():
     ax = fig.gca(projection='3d')
 
 
-    # PLOT THE PLANE!!!
+    # Plot the plane!!!
     plane_pts = []
     for ii in np.linspace(-0.2, 0.2, 10):
         for jj in np.linspace(-0.2, 0.2, 10):
